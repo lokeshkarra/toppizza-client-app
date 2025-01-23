514cb62a6f41336afe625b3eda95bb28617cb108
Lokeshwar Reddy Karra <100716442+lokeshkarra@users.noreply.github.com>
2025-01-23 23:44:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,31 +3,14 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
+  
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:3000/",
         changeOrigin: true,
-      },
-      "/public": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
+      }
     },
   },
-  plugins: [
-    TanStackRouterVite(),
-    react({
-      babel: {
-        plugins: [
-          [
-            "babel-plugin-react-compiler",
-            {
-              target: "19",
-            },
-          ],
-        ],
-      },
-    }),
-  ],
+  plugins: [TanStackRouterVite(), react()]
 });
