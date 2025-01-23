ed835bcbf11ab58a390864d311da064c845227c7
Lokeshwar Reddy Karra <100716442+lokeshkarra@users.noreply.github.com>
2025-01-23 21:02:51 +0530

    chore: initialize project structure

---
+++ src/routes/index.lazy.jsx
@@ -0,0 +1,27 @@
+import { createLazyFileRoute, Link } from "@tanstack/react-router";
+
+export const Route = createLazyFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  return (
+    <div className="index">
+      <div className="index-brand">
+        <h1>Top Pizza</h1>
+        <p>Art at a location</p>
+      </div>
+      <ul>
+        <li>
+          <Link to="/order">Order</Link>
+        </li>
+        <li>
+          <Link to="/past">Past Orders</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
