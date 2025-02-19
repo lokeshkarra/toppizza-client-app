f43130741514e1866881d4c121c47c7fba05c898
lokeshkarra <lokeshwarreddy.karra@gmail.com>
2025-02-20 00:54:07 +0530

    fix: userId based orders and past orders

--- src/api/getPastOrder.js
+++ src/api/getPastOrder.js
@@ -1,6 +1,10 @@
 export default async function getPastOrders(order) {
+  const userId = localStorage.getItem("user_id");
   const apiUrl = import.meta.env.VITE_API_URL;
-  const response = await fetch(`${apiUrl}/api/past-order/${order}`);
+  const response = await fetch(`${apiUrl}/api/past-order/${order}`, {
+    method: "GET",
+    headers: { "x-user-id": userId },
+  });
   const data = await response.json();
   return data;
 }
